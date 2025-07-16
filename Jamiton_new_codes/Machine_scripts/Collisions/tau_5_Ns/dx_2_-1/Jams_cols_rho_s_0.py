e8e49c1ce04c737f04d31b5047236c2bb1b6f530
ElTapias <sebastian.tapia.st@gmail.com>
2025-07-16 14:07:48 +0200

    Add simulation files

---
+++ Jamiton_new_codes/Machine_scripts/Collisions/tau_5_Ns/dx_2_-1/Jams_cols_rho_s_0.py
@@ -0,0 +1,80 @@
+import sys
+sys.path.append("../../../../")
+
+from Config import *
+from Utilities import *
+from Solver import *
+from Jam_generator import *
+from Functions import *
+from Collisions import *
+
+tau = 5
+j = 0
+
+base_folder = "Colls_{j}_tau_{tau}".format(j=j, tau=tau)
+create_folder(base_folder)
+base_path = base_folder + "/"
+
+rho_base_folder = base_path + "rho"
+create_folder(rho_base_folder)
+rho_base_path = rho_base_folder + "/"
+
+y_base_folder = base_path + "y"
+create_folder(y_base_folder)
+y_base_path = y_base_folder + "/"
+
+t_base_folder = base_path + "t"
+create_folder(t_base_folder)
+t_base_path = t_base_folder + "/"
+
+rhos_to_test, rho_s_test, v_minus = get_rho_s_tests()
+rho_s_test = rho_s_test/rho_max
+tf_test = 60
+
+total = int(len(rhos_to_test)/4)
+rhos_to_collide = rhos_to_test[j*total:(j+1)*total]
+
+# Test Jamiton
+x_minus_t, x_plus_t, sol_v_t, sol_v_eta_t, m_t, s_t, values_v_t = jam_values(tau, rho_s_test, tf_test, v_minus=v_minus, print_pars=False)
+sol_rho_t = lambda x: 1/sol_v_t.sol(x)[0]
+sol_u_t = lambda x: rho_to_u(sol_rho_t(x), m_t, s_t)
+
+jam_test_data = {"x_minus": x_minus_t, "x_plus": x_plus_t, "sol_rho":sol_rho_t, "sol_u":sol_u_t}
+
+# Discretization
+s_max = get_s_max()
+dx = 2**(-1)
+dt = dx/(2.1*s_max)
+
+for j in range(len(rhos_to_collide)):
+    rho_s = rhos_to_collide[j]/rho_max
+    print("Collision ", j, " of ", len(rhos_to_collide))
+
+    if rho_s < 0.512:
+        tf = 250
+    else:
+        tf = 1000
+
+    Q_col, x = Q_0_collide_fix(dx, tau, jam_test_data, rho_s, v_minus, tf, print_pars=False)
+    T = 40
+    if np.fabs(rho_s - rho_s_test)< 0.04:
+        T = 120
+
+    # Simulation
+    Nt = int(T/dt) + 1
+
+    t, rho, y = solve_ARZ(Q_col, x, tau, T, Nt, F_HLL_rho, F_HLL_y, s_max, as_array=False, print_step=False)
+
+    save_file(rho, rho_base_path+"rho_coll_{}".format(round(rho_s, 6)))
+    save_file(y, y_base_path+"y_coll_{}".format(round(rho_s, 6)))
+    save_file(t, t_base_path+"t_coll_{}".format(round(rho_s, 6)))
+
+
+
+
+
+
+
+
+
+
---
+++ Jamiton_new_codes/Machine_scripts/Errors/LxF/Errors_LxF_rho_s_0.433_tau_5_T_2.py
@@ -0,0 +1,73 @@
+import sys
+sys.path.append("../../../")
+
+from Config import *
+from Utilities import *
+from Solver import *
+from Jam_generator import *
+from Functions import *
+
+s_max = get_s_max()
+
+rho_s = 0.433
+v_minus = 26
+tau = 5
+dxs = [(2)**(-j)/10 for j in range(8)]
+T = 2
+
+base_folder = "rho_s_{rho}_Final_time_{T}".format(rho=rho_s, T=T)
+create_folder(base_folder)
+base_path = base_folder + "/"
+
+print("Solving for rho_s ", rho_s)
+
+
+tau_folder = base_path + "tau_{}".format(tau)
+create_folder(tau_folder)
+tau_path = tau_folder + "/"
+
+#rhos = np.zeros(len(dxs))
+#us = np.zeros(len(dxs))
+
+print("Creating Jamiton rho_s = ", round(rho_s, 4), " ...")
+
+x_minus, x_plus, sol_v, sol_v_eta, m, s, values_v = jam_values(tau, rho_s, 60, v_minus=v_minus)
+
+sol_v_filename = tau_path + "sol_v.pck"
+sol_v_eta_filename = tau_path + "sol_v_eta.pck"
+
+save_func_dic(sol_v, sol_v_filename)
+save_func_dic(sol_v_eta, sol_v_eta_filename)
+
+sol_rho = lambda x: 1/sol_v.sol(x)[0]
+sol_u = lambda x: rho_to_u(sol_rho(x), m, s)
+
+sol_rho_per = rho_per_gen(sol_rho, x_plus, x_minus)
+sol_u_per = u_per_gen(sol_u, x_plus, x_minus)
+
+L = np.abs(x_minus-x_plus)
+
+for n in range(len(dxs)):
+    dx = dxs[n]
+    dt = dx/(2.1*s_max)
+
+    print("Solving for dx ", dx)
+    N = int(L/dx + 1)
+    Nt = int(T/dt + 1)
+
+    x = np.linspace(0, L, N)
+
+    rho_0 = sol_rho_per(x)
+    u_0 = sol_u_per(x)
+    y_0 = rho_0 * (u_0 + h(rho_0))
+    Q_0 = [rho_0, y_0]
+
+    t, rho, y = solve_ARZ(Q_0, x, tau, T, Nt, F_LxF_rho, F_LxF_y, s_max, as_array=False)
+    u = y/rho - h(rho)
+
+    save_file(rho, tau_path+"rho_dx_{}".format(dx))
+    save_file(u, tau_path+"u_dx_{}".format(dx))
+
+print("Done!")
+
+
